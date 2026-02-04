6495333af904e8259a92eb5157abb8f79f2d8a8b
mistchek <zadoiannaia@gmail.com>
2026-02-04 15:35:04 +0300

    Update Main.java

--- ru/hse/lab1/Main.java
+++ ru/hse/lab1/Main.java
@@ -1,9 +1,1 @@
-package ru.hse.lab1.second;
 
-public class java_04_02_26 {
-  public static void main(String[] args){
-    Person David = new Person("David", 14);
-  
-    System.out.println(David.name);
-      }
-}
